9494586ca0d01682d19aa2ba73e2275c128053d6
d.balyaev <d.balyaev@npo-echelon.ru>
2023-02-09 18:21:45 +0300

    fix(app): added sidebar
    - Button
    
    - Theme Switcher

--- src/01-app/App.tsx
+++ src/01-app/App.tsx
@@ -4,15 +4,18 @@ import {Router} from './providers/Router';
 import {useTheme} from './providers/ThemeProvider';
 import {classNames} from '07-shared/lib/classNames/classNames';
 import {Navbar} from '04-widgets/Navbar';
+import {Sidebar} from "04-widgets/Sidebar";
 
 const App = () => {
-	const {theme, toggleTheme} = useTheme();
+	const {theme} = useTheme();
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Navbar/>
-			<button onClick={toggleTheme}>THEME</button>
-			<Router/>
+			<div className="content-page">
+				<Sidebar />
+				<Router/>
+			</div>
 		</div>
 	);
 };
