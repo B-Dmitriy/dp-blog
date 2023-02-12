3fd3642d8f1af50a5766d09cef3929cbe8d9a193
d.balyaev <d.balyaev@npo-echelon.ru>
2023-02-12 17:40:58 +0300

    feat(loaders): add loaders and not found page

--- src/01-app/providers/Router/ui/Router.tsx
+++ src/01-app/providers/Router/ui/Router.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { PageLoader } from '07-shared/ui/PageLoader/PageLoader';
 import { routesConfig } from '../lib/routerConfig';
 
 export const Router = () => (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PageLoader />}>
         <Routes>
             {Object.values(routesConfig).map((route) => (
                 <Route
---
+++ src/03-pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { classNames } from '07-shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import classes from './NotFoundPage.module.scss';
+
+interface NotFountPageProps {
+    className?: string;
+}
+
+export const NotFoundPage = ({ className }: NotFountPageProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(classes.NotFountPage, {}, [className])}>
+            {t('page_not_found')}
+        </div>
+    );
+};
