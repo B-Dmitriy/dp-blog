def15881bf523b7dbda96e1e34db0b4c389834bf
d.balyaev <d.balyaev@npo-echelon.ru>
2023-02-19 16:25:03 +0300

    fix(navbar): modify
    - remove coverage

--- src/04-widgets/Navbar/ui/Navbar.tsx
+++ src/04-widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import { ThemeSwitcher } from '05-features/ThemeSwitcher';
 import { LangSwitcher } from '05-features/LangSwitcher';
 import { AppNavLink } from '07-shared/ui/AppNavLink/AppNavLink';
 import { useTranslation } from 'react-i18next';
+import { IconLink } from '07-shared/ui/IconLink/IconLink';
 import classes from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,13 +15,23 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     return (
         <section className={classNames(classes.Navbar, {}, [className])}>
-            <div className={classes.switchers}>
-                <LangSwitcher />
-                <ThemeSwitcher />
-            </div>
-            <div className={classes.links}>
-                <AppNavLink to="/">{t('main')}</AppNavLink>
-                <AppNavLink to="/about">{t('about')}</AppNavLink>
+            <div className={classes.container}>
+                <div className={classes.routes}>
+                    <AppNavLink to="/">{t('main')}</AppNavLink>
+                    <AppNavLink to="/about">{t('about')}</AppNavLink>
+                </div>
+                <div className={classes.divider} />
+                <div className={classes.switchers}>
+                    <LangSwitcher />
+                    <div className={classes.divider} />
+                    <ThemeSwitcher />
+                </div>
+                <div className={classes.links}>
+                    <IconLink type="gh" href="ref" />
+                    <IconLink type="tg" href="ref" />
+                    <IconLink type="vk" href="ref" />
+                    <IconLink type="li" href="ref" />
+                </div>
             </div>
         </section>
     );
---
+++ src/07-shared/ui/IconLink/IconLink.tsx
@@ -0,0 +1,16 @@
+import { classNames } from '07-shared/lib/classNames/classNames';
+import { Icon } from '07-shared/ui/Icon/Icon';
+import classes from './IconLink.module.scss';
+import type { IconType } from '../Icon/Icon';
+
+interface IconLinkProps {
+    className?: string;
+    type: IconType;
+    href: string;
+}
+
+export const IconLink = ({ className, type, href }: IconLinkProps) => (
+    <div className={classNames(classes.IconLink, {}, [className])}>
+        <a href={href} aria-label="link"><Icon type={type} /></a>
+    </div>
+);
